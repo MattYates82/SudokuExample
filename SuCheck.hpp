37f93bb9a1656c9f7103ada5fd8c671b18107ba5
MattYates82 <yatesda@oregonstate.edu>
2016-03-21 21:28:39 -0400

    Committing initial header file for SuCheck class.

---
+++ SuCheck.hpp
@@ -0,0 +1,27 @@
+/*Sudoku Checker Implementation File
+**Michael Lewis and Matt Yates
+**Problem 6.14 - Elements of Programming Interviews */
+
+#include <iostream>
+#ifndef SUCHECK_HPP
+#define SUCHECK_HPP
+
+class SuCheck
+{
+
+private:
+
+	bool rowCheck, columnCheck, subBoardCheck;
+	bool boardCheck;
+	int suBoard[9][9];
+	bool SuCheck::checkRows();
+	bool SuCheck::checkColumns();
+	bool SuCheck::checkSubBoards();
+
+public:
+
+	SuCheck(const int*);
+	bool checkBoard();
+
+};
+#endif

141bd26d585561e92b02fbcf69d41d1cbc1f8120
FairWarning <lewismic@oregonstate.edu>
2016-03-23 11:35:59 -0700

    Update SuCheck.cpp
    I've had to consider several implications in the subBoard check that were a little more complicated than I had previously anticipated. I think that incrementing to each consecutive subBoard in the game board should hopefully be on the right track. The actual check performed per each subBoard was a little more difficult for me and required some more thought, so I'm less confident in that, but hopefully it is on the right track. Whereas you used the "i + 1" method in the row and column check functions, I allowed the subBoard check function to test at the i value, and compensated for this in the final if statement, where it accounts for the subBoard having exactly 1 "duplicate" value, which is in reality the subBoard counting the value for that initial time. Hopefully in practice this will work, but I won't be shocked if there is something I did not properly account for here. Let me know if you have any questions or insights, otherwise maybe we'll catch it once we start testing the class.

--- SuCheck.cpp
+++ SuCheck.cpp
@@ -86,3 +86,64 @@ bool SuCheck::checkColumns()
         }
     return columnCheck;
 }
+
+/*****************************************************************************
+*                       SuCheck::checkSubBoards                              *
+* Function which iterates through each 3 x 3 subBoard of the member 2D array *
+* suBoard, verifying that each element is in [1,9], aside from blanks        *
+* represented by 0. Additionally, the function determines if any elements in *
+* [1,9] are repeated. Returns true if all non-zero elements are in [1,9]     *
+* with no repeats.                                                           *
+*****************************************************************************/
+bool SuCheck::checkSubBoards()
+{
+    subBoardCheck = true;
+    // The rowOffset results in incrementing the subBoard by 3 rows
+    // vertically after every 3 arrays are incremented horizontally
+    // by the colOffset
+    for (int rowOffset = 0; rowOffset < 7; rowOffset += 3)
+    {
+        // The colOffset results in incrementing each subBoard to the
+        // next subBoard to the right (by three columns), and for
+        // every 3rd subBoard, back to the subBoard in the first
+        // column (and down by 3 rows per the rowOffset.
+        for (int colOffset = 0; colOffset < 7; colOffset += 3)
+        {
+            // Adding the rowOffset to the initialization and test
+            // expressions of this for loop will allow for proper
+            // incrementing of the subBoard by row.
+            for (int row = (0 + rowOffset); row < (3 + rowOffset); row++)
+            {
+                // Adding the rowOffset to the initialization and test
+                // expressions of this for loop will allow for proper
+                // incrementing of the subBoard by row.
+                for (int col = (0 + colOffset); col < (3 + rowOffset); col++)
+                {
+                    // Determine if there are any elements not in [0,9]
+                    if (suBoard[row][col] < 0 || suBoard[row][col] > 9)
+                    {
+                        subBoardCheck = false;
+                    }
+                    // Increment by each row of subBoard.
+                    for (int i = (row + rowOffset); i < (3 + rowOffset); i++)
+                    {
+                        // Increment through each column of each row of subBoard. 
+                        for (int j = (col + colOffset); j < (3 + colOffset); j++)
+                        {
+                            // Testing value "!= 1" instead of 0 because we must be able to
+                            // loop back to the first column of the subBoard for each of its
+                            // rows, therefore we are not using "i + 1" in the for loop and
+                            // therefore the subBoard check will match exactly for its 
+                            // starting value.
+                            if (suBoard[row][col] == (suBoard[i][j] && suBoard[row][col] != 1))
+                            {
+                                subBoardCheck = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return subBoardCheck;
+}
